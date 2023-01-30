f3bcdd8d31d7367c68c3d9db21f9b95b91ec9619
Mark Vujevits <vujevits.mark@gmail.com>
2023-01-30 17:56:40 +0100

    use vm.mocks

--- test/Unlocks.t.sol
+++ test/Unlocks.t.sol
@@ -1,20 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 
-import { console2 } from "forge-std/console2.sol";
 import { Unlocks } from "core/unlocks/Unlocks.sol";
+import { Router } from "core/router/Router.sol";
+
 import "forge-std/Test.sol";
 
 // solhint-disable func-name-mixedcase
 contract UnlockTest is Test {
-  Unlocks internal unlocks;
-  address internal receiver = vm.addr(0xf00);
+  Unlocks private unlocks;
+  address private receiver = vm.addr(0xf00);
+  address private router = vm.addr(0xb33f);
 
   function setUp() public {
-    unlocks = new Unlocks(address(this));
+    unlocks = new Unlocks(router);
+  }
+
+  function test_Metadata() public {
+    assertEq(unlocks.name(), "Tenderize Unlocks");
+    assertEq(unlocks.symbol(), "TUNL");
   }
 
   function testCreateUnlock_SuccessReturnsEncodedTokenId() public {
+    mockIsTenderizer(true);
     uint256 tokenId = unlocks.createUnlock(receiver, 1);
     (address tenderizer, uint256 decodedLockIndex) = _decodeTokenId(tokenId);
 
@@ -24,31 +32,36 @@ contract UnlockTest is Test {
 
   function testCreateUnlock_SuccessIncreasesUserBalance() public {
     uint256 balanceBefore = unlocks.balanceOf(receiver);
+    mockIsTenderizer(true);
     unlocks.createUnlock(receiver, 1);
 
     assertEq(unlocks.balanceOf(receiver), balanceBefore + 1, "user balance should increase by 1");
   }
 
   function testCreateUnlock_SuccessSetsTokenOwnership() public {
+    mockIsTenderizer(true);
     uint256 tokenId = unlocks.createUnlock(receiver, 1);
 
     assertEq(unlocks.ownerOf(tokenId), receiver, "owner should be the receiver");
   }
 
   function testCreateUnlock_FailureNotATenderizer() public {
-    vm.prank(address(receiver));
+    mockIsTenderizer(false);
 
     // TODO: how to expect specific error events
     vm.expectRevert();
-    uint256 tokenId = unlocks.createUnlock(receiver, 1);
+    unlocks.createUnlock(receiver, 1);
   }
 
   function testCreateUnlock_FailureTooLargeId() public {
+    mockIsTenderizer(true);
+
     vm.expectRevert(stdError.arithmeticError);
     unlocks.createUnlock(receiver, type(uint96).max + 1);
   }
 
   function testUseUnlock_SuccessDecreasesUserBalance() public {
+    mockIsTenderizer(true);
     unlocks.createUnlock(receiver, 1);
     uint256 balanceBefore = unlocks.balanceOf(receiver);
 
@@ -58,6 +71,7 @@ contract UnlockTest is Test {
   }
 
   function testUseUnlock_SuccessRemovesTokenOwnership() public {
+    mockIsTenderizer(true);
     uint256 tokenId = unlocks.createUnlock(receiver, 1);
     unlocks.useUnlock(receiver, 1);
 
@@ -66,11 +80,12 @@ contract UnlockTest is Test {
   }
 
   function testUseUnlock_FailureNotATenderizer() public {
+    mockIsTenderizer(true);
     unlocks.createUnlock(receiver, 1);
 
     // TODO: how to expect specific error events
     vm.expectRevert();
-    vm.prank(address(receiver));
+    mockIsTenderizer(false);
     unlocks.useUnlock(receiver, 1);
   }
 
@@ -84,8 +99,7 @@ contract UnlockTest is Test {
     return (address(bytes20(bytes32(tokenId))), uint96(bytes12(bytes32(tokenId) << 160)));
   }
 
-  // Router mock calls
-  function isTenderizer(address tenderizer) external view returns (bool) {
-    return tenderizer == address(this);
+  function mockIsTenderizer(bool v) private {
+    vm.mockCall(router, abi.encodeWithSelector(Router.isTenderizer.selector), abi.encode(v));
   }
 }
