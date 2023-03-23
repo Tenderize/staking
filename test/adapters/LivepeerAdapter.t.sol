60e56558b0e812aa2425d8fc620b68e428346a17
Mark Vujevits <vujevits.mark@gmail.com>
2023-03-23 13:36:19 +0100

    improve testFuzz_UnlockMaturity

--- test/adapters/LivepeerAdapter.t.sol
+++ test/adapters/LivepeerAdapter.t.sol
@@ -17,10 +17,11 @@ import { ISwapRouter } from "core/adapters/interfaces/ISwapRouter.sol";
 import { IWETH9 } from "core/adapters/interfaces/IWETH9.sol";
 import { LivepeerAdapter } from "core/adapters/LivepeerAdapter.sol";
 import { IERC20 } from "core/interfaces/IERC20.sol";
+import { TestHelpers } from "test/helpers/Helpers.sol";
 
 // solhint-disable func-name-mixedcase
 
-contract LivepeerAdapterTest is Test, LivepeerAdapter {
+contract LivepeerAdapterTest is Test, LivepeerAdapter, TestHelpers {
     address private validator = vm.addr(1);
     address private livepeerBonding = 0x35Bcf3c30594191d53231E4FF333E8A770453e40;
     address private livepeerRounds = 0xdd6f56DcC28D3F5f27084381fE8Df634985cc39f;
@@ -30,25 +31,8 @@ contract LivepeerAdapterTest is Test, LivepeerAdapter {
     uint24 private constant UNISWAP_POOL_FEE = 10_000;
 
     uint256 private constant WITHDRAW_ROUND = 10;
-
+    uint256 private MAX_UINT_SQRT = sqrt(type(uint256).max - 1);
     uint256 private constant ROUND_LENGTH = 25;
-    uint256 private CURRENT_ROUND_START_BLOCK;
-
-    function setUp() public {
-        vm.roll(500);
-        CURRENT_ROUND_START_BLOCK = block.number - 20;
-        vm.mockCall(
-            livepeerBonding,
-            abi.encodeWithSelector(ILivepeerBondingManager.getDelegatorUnbondingLock.selector),
-            abi.encode(0, WITHDRAW_ROUND)
-        );
-
-        vm.mockCall(
-            livepeerRounds,
-            abi.encodeWithSelector(ILivepeerRoundsManager.currentRoundStartBlock.selector),
-            abi.encode(CURRENT_ROUND_START_BLOCK)
-        );
-    }
 
     function testFuzz_PreviewDeposit(uint256 assets) public {
         assertEq(this.previewDeposit(assets), assets);
@@ -83,18 +67,32 @@ contract LivepeerAdapterTest is Test, LivepeerAdapter {
     }
 
     function testFuzz_UnlockMaturity(uint256 currentRound) public {
+        vm.roll(rand(1, 1, 100, MAX_UINT_SQRT));
+        uint256 currentRoundStartBlock = block.number - 20;
+        vm.mockCall(
+            livepeerBonding,
+            abi.encodeWithSelector(ILivepeerBondingManager.getDelegatorUnbondingLock.selector),
+            abi.encode(0, WITHDRAW_ROUND)
+        );
+
+        vm.mockCall(
+            livepeerRounds,
+            abi.encodeWithSelector(ILivepeerRoundsManager.currentRoundStartBlock.selector),
+            abi.encode(currentRoundStartBlock)
+        );
+
         uint256 unlockID = 0;
         uint256 maturity;
-        uint256 blockRemainingInCurrentRound = ROUND_LENGTH - (block.number - CURRENT_ROUND_START_BLOCK);
+        uint256 blockRemainingInCurrentRound = ROUND_LENGTH - (block.number - currentRoundStartBlock);
         vm.mockCall(livepeerRounds, abi.encodeWithSelector(ILivepeerRoundsManager.currentRound.selector), abi.encode(currentRound));
         vm.mockCall(livepeerRounds, abi.encodeWithSelector(ILivepeerRoundsManager.roundLength.selector), abi.encode(ROUND_LENGTH));
 
-        this.unlockMaturity(unlockID);
-
         if (WITHDRAW_ROUND > currentRound) {
             maturity = ROUND_LENGTH * (WITHDRAW_ROUND - currentRound - 1) + blockRemainingInCurrentRound;
+            assertEq(this.unlockMaturity(unlockID), maturity, "incorrect maturity");
+        } else {
+            assertEq(this.unlockMaturity(unlockID), maturity, "maturity should be 0");
         }
-        assertEq(this.unlockMaturity(unlockID), maturity);
     }
 
     function test_Stake() public {
