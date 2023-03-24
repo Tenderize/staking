6c53788815fd4c8084179b09c2c81a538c24d655
kyriediculous <vergauwennico@gmail.com>
2023-03-24 13:41:05 +0100

    use clone

--- src/swap/Metapool.sol
+++ src/swap/Metapool.sol
@@ -13,6 +13,7 @@ import { ERC1155 } from "solmate/tokens/ERC1155.sol";
 import { ERC20 } from "solmate/tokens/ERC20.sol";
 import { SafeTransferLib } from "solmate/utils/SafeTransferLib.sol";
 import { SafeCastLib } from "solmate/utils/SafeCastLib.sol";
+import { Clone } from "clones/Clone.sol";
 import { ClonesWithImmutableArgs } from "clones/ClonesWithImmutableArgs.sol";
 import { SD59x18, sd, pow, div, fromSD59x18, E, wrap, unwrap, lte, gte, uUNIT, UNIT } from "prb-math/SD59x18.sol";
 
@@ -30,7 +31,21 @@ struct Pool {
     LPToken lpToken;
 }
 
-contract Metapool is Multicall, SelfPermit {
+contract MetapoolImmutableArgs is Clone {
+    function underlying() public pure returns (address) {
+        return _getArgAddress(0); // start: 0 end: 19
+    }
+
+    function _router() internal pure returns (address) {
+        return _getArgAddress(20); // start: 20 end: 39
+    }
+
+    function _lpToken() internal pure returns (address) {
+        return _getArgAddress(40); // start: 40 end: 59
+    }
+}
+
+contract Metapool is MetapoolImmutableArgs, Multicall, SelfPermit {
     using ClonesWithImmutableArgs for address;
     using SafeTransferLib for ERC20;
     using SafeCastLib for uint256;
@@ -44,11 +59,6 @@ contract Metapool is Multicall, SelfPermit {
     event Withdraw(address indexed asset, address indexed to, uint128 amount, uint256 lpSharesBurnt);
     event Swap(address indexed asset, address indexed caller, address toAsset, uint256 inAmount, uint128 outAmount);
 
-    // TODO: Convert to immutable args
-    address private immutable UNDERLYING;
-    address private immutable LP_TOKEN_IMPLEMENTATION;
-    address private immutable ROUTER;
-
     // Fee parameters
     uint128 private constant FEE = 0.003e18;
     uint128 private constant FEE_DENOMINATOR = 1e18;
@@ -59,150 +69,175 @@ contract Metapool is Multicall, SelfPermit {
     SD59x18 private constant K = SD59x18.wrap(0.005e18);
     SD59x18 private constant N = SD59x18.wrap(10e18);
 
-    // TODO: Convert to EIP-1967 structured storage
-    uint256 public totalAssets;
-    uint256 public totalLiabilities;
-    mapping(address => Pool) pools;
+    uint256 private constant SSLOT = uint256(keccak256("xyz.tenderize.swap.storage.location")) - 1;
 
-    constructor(address _underlying, address _lpToken, address _router) {
-        UNDERLYING = _underlying;
-        LP_TOKEN_IMPLEMENTATION = _lpToken;
-        ROUTER = _router;
+    struct Data {
+        uint128 totalAssets;
+        uint128 totalLiabilities;
+        mapping(address => Pool) pools;
     }
 
-    function getPool(address asset) public view returns (Pool memory) {
-        return pools[asset];
+    function _loadStorageSlot() internal pure returns (Data storage s) {
+        uint256 slot = SSLOT;
+
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            s.slot := slot
+        }
     }
 
-    function deposit(address asset, uint128 amount) public {
-        Pool memory pool = pools[asset];
+    function pool(address asset) external view returns (Pool memory) {
+        Data storage s = _loadStorageSlot();
 
-        _checkRebase(asset, pool);
+        return s.pools[asset];
+    }
+
+    function totalAssets() external view returns (uint128) {
+        Data storage s = _loadStorageSlot();
+
+        return s.totalAssets;
+    }
+
+    function totalLiabilities() external view returns (uint128) {
+        Data storage s = _loadStorageSlot();
+
+        return s.totalLiabilities;
+    }
+
+    function deposit(address asset, uint128 amount) external {
+        Data storage s = _loadStorageSlot();
+
+        Pool memory p = s.pools[asset];
 
         // Create pool if it doesn't exist
-        if (address(pool.lpToken) == address(0)) {
-            pool = _createPool(asset);
+        if (address(p.lpToken) == address(0)) {
+            p = _createPool(asset);
         }
 
+        _checkRebase(asset, p);
+
         // Calculate LP shares to mint
-        uint256 lpShares = pool.liabilities == 0 ? amount : amount * pool.lpToken.totalSupply() / pool.liabilities;
+        uint256 lpShares = p.liabilities == 0 ? amount : amount * p.lpToken.totalSupply() / p.liabilities;
 
         // Update pool state
-        pool.assets += amount;
-        pool.liabilities += amount;
+        p.assets += amount;
+        p.liabilities += amount;
 
         // Update metapool state
-        totalAssets += amount;
-        totalLiabilities += amount;
+        s.totalAssets += amount;
+        s.totalLiabilities += amount;
 
         // Transfer tokens to pool
         ERC20(asset).safeTransferFrom(msg.sender, address(this), amount);
 
         // Mint LP shares to depositor
-        pool.lpToken.mint(msg.sender, lpShares);
+        p.lpToken.mint(msg.sender, lpShares);
 
-        pools[asset] = pool;
+        s.pools[asset] = p;
 
         emit Deposit(asset, msg.sender, amount, lpShares);
     }
 
-    function withdraw(address asset, uint128 amount) public {
-        Pool memory pool = pools[asset];
+    function withdraw(address asset, uint128 amount) external {
+        Data storage s = _loadStorageSlot();
 
-        _checkRebase(asset, pool);
+        Pool memory p = s.pools[asset];
+
+        _checkRebase(asset, p);
 
         // Revert if pool has insufficient assets
-        if (pool.assets < amount) {
-            revert InsufficientAssets(amount, pool.assets);
+        if (p.assets < amount) {
+            revert InsufficientAssets(amount, p.assets);
         }
 
         // Calculate LP shares to burn
-        uint256 lpShares = amount * pool.lpToken.totalSupply() / pool.liabilities;
+        uint256 lpShares = amount * p.lpToken.totalSupply() / p.liabilities;
 
         // Update pool state
-        pool.assets -= amount;
-        pool.liabilities -= amount;
+        p.assets -= amount;
+        p.liabilities -= amount;
 
         // Update metapool state
-        totalAssets -= amount;
-        totalLiabilities -= amount;
+        s.totalAssets -= amount;
+        s.totalLiabilities -= amount;
 
         // Burn LP shares from depositor
-        pool.lpToken.burn(msg.sender, lpShares);
+        p.lpToken.burn(msg.sender, lpShares);
 
         // Transfer tokens to depositor
         ERC20(asset).safeTransfer(msg.sender, amount);
 
-        pools[asset] = pool;
+        s.pools[asset] = p;
 
         emit Withdraw(asset, msg.sender, amount, lpShares);
     }
 
     function withdrawOther(address asset, address otherAsset, uint128 amount) external returns (uint128 out) {
-        Pool memory pool = pools[asset];
-        Pool memory otherPool = pools[otherAsset];
+        Data storage s = _loadStorageSlot();
 
-        _checkRebase(asset, pool);
-        _checkRebase(otherAsset, otherPool);
+        Pool memory p = s.pools[asset];
+        Pool memory op = s.pools[otherAsset];
+
+        _checkRebase(asset, p);
+        _checkRebase(otherAsset, op);
 
         // Can only withdrawOther if `asset` has a score less than 0
-        if (unwrap(_score(pool.assets, pool.liabilities)) >= 0) revert();
+        if (unwrap(_score(p.assets, p.liabilities)) >= 0) revert();
 
         // Withdrawing from `otherAsset` can't reduce its score below 0
-        if (otherPool.assets - otherPool.liabilities < amount) {
-            revert InsufficientAssets(amount, otherPool.assets - otherPool.liabilities);
+        if (p.assets - p.liabilities < amount) {
+            revert InsufficientAssets(amount, op.assets - op.liabilities);
         }
 
         // Calculate slippage for reducing score from `otherAsset`
-        SD59x18 s =
-            _slippage(_score(otherPool.assets, otherPool.liabilities), _score(otherPool.assets - amount, otherPool.liabilities));
+        SD59x18 sl = _slippage(_score(op.assets, op.liabilities), _score(op.assets - amount, op.liabilities));
 
-        s = wrap(1e18).sub(s);
+        sl = UNIT.sub(sl);
 
-        out = (amount * uint256(unwrap(s)) / 1e18).safeCastTo128();
+        out = (amount * uint256(unwrap(sl)) / 1e18).safeCastTo128();
 
         // Calculate LP shares to burn from depositer for `asset`
-        uint256 lpShares = amount * pool.lpToken.totalSupply() / pool.liabilities;
+        uint256 lpShares = amount * p.lpToken.totalSupply() / p.liabilities;
 
         // update liabilities for `asset`
-        pool.liabilities -= amount;
+        p.liabilities -= amount;
         // update assets for `otherAsset`
-        otherPool.assets -= uint128(out);
+        op.assets -= uint128(out);
 
         // Burn LP shares from depositor for `asset`
-        pool.lpToken.burn(msg.sender, lpShares);
+        p.lpToken.burn(msg.sender, lpShares);
 
         // Transfer tokens to depositor for `otherAsset`
         ERC20(otherAsset).safeTransfer(msg.sender, out);
 
-        pools[asset] = pool;
-        pools[otherAsset] = otherPool;
+        s.pools[asset] = p;
+        s.pools[otherAsset] = op;
 
         emit Withdraw(otherAsset, msg.sender, out, lpShares);
     }
 
     function swap(address from, address to, uint128 amount, uint128 minOut) external returns (uint128 out) {
-        Pool memory i = pools[from];
-        Pool memory j = pools[to];
+        Data storage s = _loadStorageSlot();
+
+        Pool memory i = s.pools[from];
+        Pool memory j = s.pools[to];
 
         _checkRebase(from, i);
         _checkRebase(to, j);
 
-        // calculate fee
-        uint128 fee = amount * FEE / FEE_DENOMINATOR;
-
         // quote output amount for `amount` of `from` to `to`
-        out = _quote(i, j, amount - fee);
+        uint128 fee;
+        (out, fee) = _quote(i, j, amount);
 
         // Revert if slippage threshold is exceeded, i.e. if `out` is less than `minOut`
         if (out < minOut) revert SlippageThresholdExceeded(out, minOut);
 
         // Add `amount` to `from` pool assets
         i.assets += amount;
-        //  Add `fee` to `from` pool liabilities
-        i.liabilities += fee;
-        // Subtract `out` from `to` pool assets
+        // Subtract `out` to `to` pool assets
         j.assets -= out;
+        //  Add `fee` to `to` pool liabilities
+        j.liabilities += fee;
 
         // Transfer `amount` of `from` to this pool
         ERC20(from).safeTransferFrom(msg.sender, address(this), amount);
@@ -210,25 +245,29 @@ contract Metapool is Multicall, SelfPermit {
         // Transfer `out` of `to` to msg.sender
         ERC20(to).safeTransfer(msg.sender, out);
 
-        pools[from] = i;
-        pools[to] = j;
+        s.pools[from] = i;
+        s.pools[to] = j;
 
         emit Swap(from, msg.sender, to, amount, out);
     }
 
     function quote(address from, address to, uint256 amount) external view returns (uint128 out) {
+        Data storage s = _loadStorageSlot();
+
         // Use a memory cache to save gas
-        Pool memory i = pools[from];
-        Pool memory j = pools[to];
-        out = _quote(i, j, amount);
+        Pool memory i = s.pools[from];
+        Pool memory j = s.pools[to];
+        (out,) = _quote(i, j, amount);
     }
 
     function score(address asset) external view returns (SD59x18) {
-        Pool memory pool = pools[asset];
-        return _score(pool.assets, pool.liabilities);
+        Data storage s = _loadStorageSlot();
+
+        Pool memory p = s.pools[asset];
+        return _score(p.assets, p.liabilities);
     }
 
-    function _quote(Pool memory i, Pool memory j, uint256 amount) internal pure returns (uint128 out) {
+    function _quote(Pool memory i, Pool memory j, uint256 amount) internal pure returns (uint128 out, uint128 fee) {
         // Get the slippage for increasing the score of pool `i`
         SD59x18 s_i = _slippage(_score(i.assets, i.liabilities), _score(i.assets + amount, i.liabilities));
         // Get the slippage for decreasing the score of pool `j`
@@ -239,6 +278,8 @@ contract Metapool is Multicall, SelfPermit {
 
         // Calculate the output amount as `amount * (1 - slippage)` and safecast to uint128
         out = (amount * uint256(unwrap(s)) / 1e18).safeCastTo128();
+        fee = out * FEE / 1e18;
+        out -= fee;
     }
 
     function _slippage(SD59x18 r, SD59x18 rY) internal pure returns (SD59x18) {
@@ -269,41 +310,37 @@ contract Metapool is Multicall, SelfPermit {
         }
     }
 
-    function _checkRebase(address asset, Pool memory pool) internal view {
+    function _checkRebase(address asset, Pool memory p) internal view {
         uint256 balance = ERC20(asset).balanceOf(address(this));
-        if (balance == pool.assets) {
+        if (balance == p.assets) {
             return;
-        } else if (balance > pool.assets) {
-            uint128 diff = (balance - pool.assets).safeCastTo128();
-            pool.assets = pool.assets + diff;
-            pool.liabilities += diff;
+        } else if (balance > p.assets) {
+            uint128 diff = (balance - p.assets).safeCastTo128();
+            p.assets = p.assets + diff;
+            p.liabilities += diff;
         } else {
-            uint128 diff = (pool.assets - balance).safeCastTo128();
-            pool.assets < diff ? 0 : pool.assets - diff;
-            pool.liabilities -= diff;
+            uint128 diff = (p.assets - balance).safeCastTo128();
+            p.assets < diff ? 0 : p.assets - diff;
+            p.liabilities -= diff;
         }
     }
 
-    function _createPool(address asset) internal returns (Pool memory pool) {
-        // Check that `asset` is the UNDERLYING asset for the metapool or a tenderizer
-        if (asset != UNDERLYING && !Router(ROUTER).isTenderizer(asset)) {
+    function _createPool(address asset) internal returns (Pool memory) {
+        // Check that `asset` is the underlying() asset for the metapool or a tenderizer
+        if (asset != underlying() && !Router(_router()).isTenderizer(asset)) {
             revert InvalidAsset(asset);
         }
 
-        // Check that tenderizer is for the same UNDERLYING asset as this Metapool
+        // Check that tenderizer is for the same underlying() asset as this Metapool
         {
             // Prevent a call to Tenderizer.asset() if `asset` is `underlying`
             address assetUnderlying;
-            if (asset != UNDERLYING && (assetUnderlying = Tenderizer(asset).asset()) != UNDERLYING) {
-                revert WrongMetapool(assetUnderlying, UNDERLYING);
+            if (asset != underlying() && (assetUnderlying = Tenderizer(asset).asset()) != underlying()) {
+                revert WrongMetapool(assetUnderlying, underlying());
             }
         }
 
         // Create an LP token for the new pool
-        pool = Pool({
-            lpToken: LPToken(LP_TOKEN_IMPLEMENTATION.clone(abi.encodePacked(asset, address(this)))),
-            assets: 0,
-            liabilities: 0
-        });
+        return Pool({ lpToken: LPToken(_lpToken().clone(abi.encodePacked(asset, address(this)))), assets: 0, liabilities: 0 });
     }
 }
--- test/swap/Swap.t.sol
+++ test/swap/Swap.t.sol
@@ -14,6 +14,7 @@ pragma solidity 0.8.17;
 import { Test, stdError, console } from "forge-std/Test.sol";
 import { Router } from "core/router/Router.sol";
 import { MockERC20 } from "test/helpers/MockERC20.sol";
+import { ClonesWithImmutableArgs } from "clones/ClonesWithImmutableArgs.sol";
 
 import { Metapool } from "core/swap/Metapool.sol";
 import { LPToken } from "core/swap/LpToken.sol";
@@ -21,10 +22,12 @@ import { TenderizerImmutableArgs } from "core/tenderizer/TenderizerBase.sol";
 import { SD59x18, sd, pow, fromSD59x18, E, wrap, unwrap } from "prb-math/SD59x18.sol";
 
 contract MetaPoolTest is Test {
+    using ClonesWithImmutableArgs for address;
+
     MockERC20 tokenA;
     MockERC20 tokenB;
 
-    Metapool pool;
+    Metapool mp;
 
     address router = vm.addr(1333);
 
@@ -32,65 +35,66 @@ contract MetaPoolTest is Test {
         tokenA = new MockERC20("Token A", "TKA", 18);
         tokenB = new MockERC20("Token B", "TKB", 18);
 
-        pool = new Metapool(address(tokenA), address(new LPToken()), router);
+        // default mock calls
+        vm.mockCall(router, abi.encodeWithSelector(Router.isTenderizer.selector), abi.encode(true));
+        vm.mockCall(address(tokenB), abi.encodeWithSelector(TenderizerImmutableArgs.asset.selector), abi.encode(address(tokenA)));
+
+        mp = Metapool(address(new Metapool()).clone(abi.encodePacked(address(tokenA), router, address(new LPToken()))));
     }
 
     function test_Deposit() public {
         tokenA.mint(address(this), 1000);
-        tokenA.approve(address(pool), 1000);
+        tokenA.approve(address(mp), 1000);
 
-        pool.deposit(address(tokenA), 1000);
-        assertEq(pool.totalAssets(), 1000);
-        assertEq(pool.totalLiabilities(), 1000);
-        assertEq(pool.getPool(address(tokenA)).assets, 1000);
-        assertEq(pool.getPool(address(tokenA)).liabilities, 1000);
-        assertEq(pool.getPool(address(tokenA)).lpToken.totalSupply(), 1000);
+        mp.deposit(address(tokenA), 1000);
+        assertEq(mp.totalAssets(), 1000);
+        assertEq(mp.totalLiabilities(), 1000);
+        assertEq(mp.pool(address(tokenA)).assets, 1000);
+        assertEq(mp.pool(address(tokenA)).liabilities, 1000);
+        assertEq(mp.pool(address(tokenA)).lpToken.totalSupply(), 1000);
 
-        vm.mockCall(router, abi.encodeWithSelector(Router.isTenderizer.selector), abi.encode(true));
-        vm.mockCall(address(tokenB), abi.encodeWithSelector(TenderizerImmutableArgs.asset.selector), abi.encode(address(tokenA)));
         tokenB.mint(address(this), 1000);
-        tokenB.approve(address(pool), 1000);
+        tokenB.approve(address(mp), 1000);
         vm.expectCall(router, abi.encodeCall(Router.isTenderizer, address(tokenB)));
         vm.expectCall(address(tokenB), abi.encodeCall(TenderizerImmutableArgs.asset, ()));
 
-        pool.deposit(address(tokenB), 1000);
-        assertEq(pool.totalAssets(), 2000);
-        assertEq(pool.totalLiabilities(), 2000);
-        assertEq(pool.getPool(address(tokenB)).assets, 1000);
-        assertEq(pool.getPool(address(tokenB)).liabilities, 1000);
-        assertEq(pool.getPool(address(tokenB)).lpToken.totalSupply(), 1000);
+        mp.deposit(address(tokenB), 1000);
+        assertEq(mp.totalAssets(), 2000);
+        assertEq(mp.totalLiabilities(), 2000);
+        assertEq(mp.pool(address(tokenB)).assets, 1000);
+        assertEq(mp.pool(address(tokenB)).liabilities, 1000);
+        assertEq(mp.pool(address(tokenB)).lpToken.totalSupply(), 1000);
     }
 
     function test_Deposit_ErrorInvalidAsset() public {
         tokenB.mint(address(this), 1000);
-        tokenB.approve(address(pool), 1000);
+        tokenB.approve(address(mp), 1000);
 
+        vm.clearMockedCalls();
         vm.mockCall(router, abi.encodeWithSelector(Router.isTenderizer.selector), abi.encode(false));
         vm.expectCall(router, abi.encodeCall(Router.isTenderizer, address(tokenB)));
         vm.expectRevert(abi.encodeWithSelector(Metapool.InvalidAsset.selector, address(tokenB)));
 
-        pool.deposit(address(tokenB), 1000);
+        mp.deposit(address(tokenB), 1000);
     }
 
     function test_Swap() public {
         // Set Up
         tokenA.mint(address(this), 5 ether);
-        tokenA.approve(address(pool), 5 ether);
-        pool.deposit(address(tokenA), 5 ether);
-        vm.mockCall(router, abi.encodeWithSelector(Router.isTenderizer.selector), abi.encode(true));
-        vm.mockCall(address(tokenB), abi.encodeWithSelector(TenderizerImmutableArgs.asset.selector), abi.encode(address(tokenA)));
+        tokenA.approve(address(mp), 5 ether);
+        mp.deposit(address(tokenA), 5 ether);
 
         tokenB.mint(address(this), 0.33 ether);
-        tokenB.approve(address(pool), 0.33 ether);
+        tokenB.approve(address(mp), 0.33 ether);
 
-        pool.deposit(address(tokenB), 0.33 ether);
+        mp.deposit(address(tokenB), 0.33 ether);
 
         tokenA.mint(address(this), 0.22 ether);
-        tokenA.approve(address(pool), 0.22 ether);
-        uint128 minOut = pool.quote(address(tokenA), address(tokenB), 0.03 ether);
-        uint128 out = pool.swap(address(tokenA), address(tokenB), 0.03 ether, minOut);
+        tokenA.approve(address(mp), 0.22 ether);
+        uint128 minOut = mp.quote(address(tokenA), address(tokenB), 0.03 ether);
+        uint128 out = mp.swap(address(tokenA), address(tokenB), 0.03 ether, minOut);
         console.log("out from swap ", out);
-        uint128 quoteNext = pool.quote(address(tokenB), address(tokenA), 0.03 ether);
+        uint128 quoteNext = mp.quote(address(tokenB), address(tokenA), 0.03 ether);
         console.log("Quote for next swap ", quoteNext);
         uint128 slip = (0.03 ether - out);
         console.log("diff ", slip - (quoteNext - 0.03 ether));
